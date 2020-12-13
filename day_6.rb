b94b82412dd629753733a2b4b8a3a2623dc1c021
Anderson Fernandes <fernandesanderson14@gmail.com>
2020-12-13 20:27:33 -0300

    Added part II of day 6
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- day_6.rb
+++ day_6.rb
@@ -6,16 +6,18 @@ groups = File.readlines('./day_6.in').each_with_object([[]]) do |line, groups_da
   if line.chomp.empty?
     groups_data << []
   else
-    groups_data.last.push(line.chomp)
+    groups_data.last.push(line.chomp.chars)
   end
 
   groups_data
 end
 
 groups.each do |group|
-  Set.new(group.reduce(&:concat).chars).each do |question|
-    questions[question] += 1
-  end
+  group
+    .reduce(('a'..'z').to_a) { |acc, g| acc.intersection(g) }
+    .each do |question|
+      questions[question] += 1
+    end
 end
 
 puts(questions.sum { |_, v| v })
